7907697f4d5979fefa06068b01496f65ed483901
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-05 02:54:16 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -7,7 +7,12 @@ class Customer
     @age = age
     @@all << self
   end
+  
   def self.all
     @@all
+  end
+  
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
   end
 end
